b06e87f50523610a01b5b30653b39730ead25da3
Eddy <id.hemedy98@gmail.com>
2024-01-09 03:59:32 +0200

    Working on filters

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -1,8 +1,11 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link'
 
 import classes from './index.module.scss'
 import { Category, Media } from '../../../../payload/payload-types'
+import { useFilter } from '../../../_providers/Filters'
 
 type CategoryCardProps = {
   category: Category
@@ -10,12 +13,14 @@ type CategoryCardProps = {
 const CategoryCard = ({ category }: CategoryCardProps) => {
 
   const media = category.media as Media
+  const { setCategoryFilters } = useFilter()
 
   return (
     <Link
       href='/products'
       className={classes.card}
       style={{ backgroundImage: `url(${media.url})` }}
+      onClick={() => setCategoryFilters([category.id])}
     >
       <p className={classes.title}>{category.title}</p>
     </Link>
