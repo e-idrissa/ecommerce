cd55f3dd081e8f2458f67562aab813073d2bae00
Eddy <id.hemedy98@gmail.com>
2024-01-10 14:54:18 +0200

    Working on CartItem page

--- src/app/(pages)/cart/CartPage/index.tsx
+++ src/app/(pages)/cart/CartPage/index.tsx
@@ -14,6 +14,7 @@ import { useAuth } from '../../../_providers/Auth'
 import { useCart } from '../../../_providers/Cart'
 
 import classes from './index.module.scss'
+import CartItem from '../CartItem'
 
 export const CartPage: React.FC<{
   settings: Settings
@@ -80,62 +81,14 @@ export const CartPage: React.FC<{
                   const metaImage = meta?.image
 
                   return (
-                    <Fragment key={index}>
-                      <div className={classes.row}>
-                        <Link href={`/products/${product.slug}`} className={classes.mediaWrapper}>
-                          {!metaImage && <span className={classes.placeholder}>No image</span>}
-                          {metaImage && typeof metaImage !== 'string' && (
-                            <Media
-                              className={classes.media}
-                              imgClassName={classes.image}
-                              resource={metaImage}
-                              fill
-                            />
-                          )}
-                        </Link>
-                        <div className={classes.rowContent}>
-                          {!stripeProductID && (
-                            <p className={classes.warning}>
-                              {
-                                'This product is not yet connected to Stripe. To link this product, '
-                              }
-                              <Link
-                                href={`${process.env.NEXT_PUBLIC_SERVER_URL}/admin/collections/products/${id}`}
-                              >
-                                edit this product in the admin panel
-                              </Link>
-                              {'.'}
-                            </p>
-                          )}
-                          <h5 className={classes.title}>
-                            <Link href={`/products/${product.slug}`} className={classes.titleLink}>
-                              {title}
-                            </Link>
-                          </h5>
-                          <div className={classes.actions}>
-                            <label>
-                              Quantity &nbsp;
-                              <input
-                                type="number"
-                                className={classes.quantity}
-                                // fallback to empty string to avoid uncontrolled input error
-                                // this allows the user to user their backspace key to clear the input
-                                value={typeof quantity === 'number' ? quantity : ''}
-                                onChange={e => {
-                                  addItemToCart({
-                                    product,
-                                    quantity: Number(e.target.value),
-                                  })
-                                }}
-                              />
-                            </label>
-                            <RemoveFromCartButton product={product} />
-                          </div>
-                          <Price product={product} button={false} quantity={quantity} />
-                        </div>
-                      </div>
-                      {!isLast && <HR />}
-                    </Fragment>
+                    <CartItem
+                      product={product}
+                      title={title}
+                      metaImage={metaImage}
+                      index={index}
+                      qty={quantity}
+                      addItemToCart={addItemToCart}
+                    />
                   )
                 }
                 return null
