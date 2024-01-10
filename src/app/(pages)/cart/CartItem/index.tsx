0d6bde9fe310af333abdf01e148d09f6dc1626e3
Eddy <id.hemedy98@gmail.com>
2024-01-10 15:30:53 +0200

    Finalising Cart item page

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -11,47 +11,61 @@ const CartItem = ({ product, title, metaImage, qty, addItemToCart, index }) => {
 
   const [quantity, setQuantity] = useState(qty)
 
-  const incrementQty = () => {}
-  const decrementQty = () => {}
-  const enterQty = () => {}
+  const incrementQty = () => {
+    const updatedQty = quantity + 1
+    setQuantity(updatedQty)
+    addItemToCart({ product, quantity: Number(updatedQty) })
+  }
+
+  const decrementQty = () => {
+    const updatedQty = quantity > 1 ? quantity - 1 : 1
+    setQuantity(updatedQty)
+    addItemToCart({ product, quantity: Number(updatedQty) })
+  }
+
+  const enterQty = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedQty = Number(e.target.value)
+    setQuantity(updatedQty)
+    addItemToCart({ product, quantity: Number(updatedQty) })
+  }
 
   return (
-    <li className={classes.item} key={index}>
+    <li className={classes.item} key={title}>
       <Link href={`/products/${product.slug}`} className={classes.mediaWrapper}>
-        {!metaImage
-          ? <span>No Image</span>
-          : typeof metaImage !== 'string' && (
-            <Media className={classes.media} imgClassName={classes.image} resource={metaImage} fill/>
-          )
-        }
+        {!metaImage && <span>No image</span>}
+        {metaImage && typeof metaImage !== 'string' && (
+          <Media className={classes.media} imgClassName={classes.image} resource={metaImage} fill />
+        )}
       </Link>
 
       <div className={classes.itemDetails}>
         <div className={classes.titleWrapper}>
           <h6>{title}</h6>
-          <Price product={product} button={false}/>
+          <Price product={product} button={false} />
         </div>
 
         <div className={classes.quantity}>
           <div className={classes.quantityBtn} onClick={decrementQty}>
             <Image
-              src='/assets/icons/minus.svg'
-              alt='minus'
+              src="/assets/icons/minus.svg"
+              alt="minus"
               width={24}
               height={24}
               className={classes.qtnBt}
             />
           </div>
+
           <input
             type="text"
             className={classes.quantityInput}
             value={quantity}
             onChange={enterQty}
           />
+
           <div className={classes.quantityBtn} onClick={incrementQty}>
             <Image
-              src='/assets/icons/plus.svg'
-              alt='plus'
+              src="/assets/icons/plus.svg"
+              alt="plus"
               width={24}
               height={24}
               className={classes.qtnBt}
@@ -60,8 +74,8 @@ const CartItem = ({ product, title, metaImage, qty, addItemToCart, index }) => {
         </div>
       </div>
 
-      <div className={classes.subTotalWrapper}>
-        <Price product={product} button={false} quantity={quantity}/>
+      <div className={classes.subtotalWrapper}>
+        <Price product={product} button={false} quantity={quantity} />
         <RemoveFromCartButton product={product} />
       </div>
     </li>
--- src/app/(pages)/cart/CartPage/index.tsx
+++ src/app/(pages)/cart/CartPage/index.tsx
@@ -55,52 +55,69 @@ export const CartPage: React.FC<{
               )}
             </div>
           ) : (
-            <div className={classes.items}>
-              <div className={classes.itemsTotal}>
-                {`There ${cart?.items?.length === 1 ? 'is' : 'are'} ${cart?.items?.length} item${
-                  cart?.items?.length === 1 ? '' : 's'
-                } in your cart.`}
-                {!user && (
-                  <Fragment>
-                    {' '}
-                    <Link href={`/login?redirect=%2Fcart`}>Log in</Link>
-                    {` to save your progress.`}
-                  </Fragment>
-                )}
+            <div className={classes.cartWrapper}>
+              <div>
+                {/* Cart list header */}
+                <div className={classes.header}>
+                  <p>Produscts</p>
+                  <div className={classes.headerItemDetails}>
+                    <p></p>
+                    <p></p>
+                    <p>Quantity</p>
+                  </div>
+                  <p className={classes.headersubtotal}>Subtotal</p>
+                </div>
+                {/* Cart items list */}
+                <ul className={classes.itemsList}>
+                  {cart?.items?.map((item, index) => {
+                    if (typeof item.product === 'object') {
+                      const {
+                        quantity,
+                        product,
+                        product: { id, title, meta, stripeProductID },
+                      } = item
+
+                      const isLast = index === (cart?.items?.length || 0) - 1
+
+                      const metaImage = meta?.image
+
+                      return (
+                        <CartItem
+                          product={product}
+                          title={title}
+                          metaImage={metaImage}
+                          index={index}
+                          qty={quantity}
+                          addItemToCart={addItemToCart}
+                        />
+                      )
+                    }
+                    return null
+                  })}
+                </ul>
               </div>
-              {cart?.items?.map((item, index) => {
-                if (typeof item.product === 'object') {
-                  const {
-                    quantity,
-                    product,
-                    product: { id, title, meta, stripeProductID },
-                  } = item
 
-                  const isLast = index === (cart?.items?.length || 0) - 1
 
-                  const metaImage = meta?.image
+              <div className={classes.summary}>
+                <div className={classes.row}>
+                  <h6 className={classes.cartTotal}>Summary</h6>
+                </div>
+                <div className={classes.row}>
+                  <h6 className={classes.cartTotal}>Delivery Charge</h6>
+                  <h6 className={classes.cartTotal}>$0</h6>
+                </div>
+                <div className={classes.row}>
+                  <h6 className={classes.cartTotal}>Grand Total</h6>
+                  <h6 className={classes.cartTotal}>{cartTotal.formatted}</h6>
+                </div>
+                <Button
+                  className={classes.checkoutButton}
+                  href={user ? '/checkout' : '/login?redirect=%2Fcheckout'}
+                  label={user ? 'Checkout' : 'Login to checkout'}
+                  appearance="primary"
+                />
+              </div>
 
-                  return (
-                    <CartItem
-                      product={product}
-                      title={title}
-                      metaImage={metaImage}
-                      index={index}
-                      qty={quantity}
-                      addItemToCart={addItemToCart}
-                    />
-                  )
-                }
-                return null
-              })}
-              <HR />
-              <h5 className={classes.cartTotal}>{`Total: ${cartTotal.formatted}`}</h5>
-              <Button
-                className={classes.checkoutButton}
-                href={user ? '/checkout' : '/login?redirect=%2Fcheckout'}
-                label={user ? 'Checkout' : 'Login to checkout'}
-                appearance="primary"
-              />
             </div>
           )}
         </Fragment>
